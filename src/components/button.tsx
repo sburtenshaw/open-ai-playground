81759a44c39dc2593dcec9632a94596fdc957918
Sean Burtenshaw <seanburtenshaw@Seans-MacBook-Pro.local>
2023-07-13 00:20:53 +0100

    Added small button variation

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,19 +1,20 @@
 import type { ButtonHTMLAttributes, ReactNode } from "react";
 
 interface PropsType extends ButtonHTMLAttributes<HTMLButtonElement> {
+  small?: boolean;
   children: ReactNode;
 }
 
-function Button({ disabled, children, ...props }: PropsType) {
+function Button({ disabled, small = false, children, ...props }: PropsType) {
   const getClassName = () => {
     const className = [
       "text-white",
       "font-medium",
       "rounded-lg",
-      "text-sm",
-      "px-5",
-      "h-12",
       "text-center",
+      small ? "text-xs" : "text-sm",
+      small ? "px-3" : "px-5",
+      small ? "h-9" : "h-12",
     ];
     if (disabled) {
       className.push("bg-blue-400", "cursor-not-allowed");
