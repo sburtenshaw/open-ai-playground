751bc3971a6bbe9f2e7b2de25cf8c5bfcfe0c6b1
Sean Burtenshaw <seanburtenshaw@Seans-MBP.lan>
2023-07-12 16:38:33 +0100

    Updated text-input component to use textarea instead of input

--- src/components/text-input.tsx
+++ src/components/text-input.tsx
@@ -1,11 +1,26 @@
-import type { InputHTMLAttributes } from "react";
+import type { DetailedHTMLProps, TextareaHTMLAttributes } from "react";
 
-function TextInput(props: InputHTMLAttributes<HTMLInputElement>) {
+function TextInput({
+  value,
+  ...props
+}: DetailedHTMLProps<
+  TextareaHTMLAttributes<HTMLTextAreaElement>,
+  HTMLTextAreaElement
+>) {
+  const calculateRows = (): number => {
+    if (!value || typeof value !== "string") {
+      return 1;
+    }
+    return (value.match(/\n/g) || []).length + 1;
+  };
+
   return (
-    <input
-      className="block w-full appearance-none rounded border border-gray-200 bg-gray-200 px-4 py-3 leading-tight text-gray-700 focus:border-gray-500 focus:bg-white focus:outline-none"
-      type="text"
+    <textarea
+      rows={calculateRows()}
       {...props}
+      value={value}
+      className="block w-full resize-none appearance-none rounded-lg border border-gray-200 bg-gray-200 px-4 py-3 leading-tight text-gray-700 focus:border-gray-500 focus:bg-white focus:outline-none"
+      style={{ minHeight: "48px" }}
     />
   );
 }
