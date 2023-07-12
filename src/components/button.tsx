e6ab81e7fadbcad9a2b1e59d450d2e059292812a
Sean Burtenshaw <seanburtenshaw@Seans-MBP.lan>
2023-07-12 16:36:53 +0100

    Updated button to include disabled state

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -4,13 +4,27 @@ interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
 }
 
-function Button({ children, ...props }: Props) {
+function Button({ disabled, children, ...props }: Props) {
+  const getClassName = () => {
+    const className = [
+      "text-white",
+      "font-medium",
+      "rounded-lg",
+      "text-sm",
+      "px-5",
+      "h-12",
+      "text-center",
+    ];
+    if (disabled) {
+      className.push("bg-blue-400", "cursor-not-allowed");
+    } else {
+      className.push("bg-blue-700", "hover:bg-blue-800", "focus:outline-none");
+    }
+    return className.join(" ");
+  };
+
   return (
-    <button
-      {...props}
-      type="button"
-      className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
-    >
+    <button {...props} type="button" className={getClassName()}>
       {children}
     </button>
   );
